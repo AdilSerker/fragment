808390f30828315a7b04d78feb12051bcb8dd96b
Adil <adilserkerov@gmail.com>
2018-07-19 00:33:27 +0300

    webpack

---
+++ src/client/index.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import { render } from 'react-dom';
+
+const root = document.getElementById('root');
+
+const Test = () => (
+    <div>
+      <p>Are you sure?</p>
+    </div>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/client/index.ts',
+  entry: './src/client/index.tsx',
+  devtool: 'source-map',
+  watch: true,
+  watchOptions: {
+    poll: 1000
+  },
   module: {
     rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
   },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: [ '.tsx', '.ts', '.js', 'JSON' ]
   },
   output: {
     filename: 'bundle.js',
